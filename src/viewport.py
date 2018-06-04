a711af49dcce3cbd5938de8d9b63033f016d15a3
Zefaxet <getmemedkid@dicksinhisan.us>
2018-06-04 11:58:45 -0500

    Working on setting the tkinter layour.

--- src/viewport.py
+++ src/viewport.py
@@ -1,5 +1,20 @@
-from tkinter import Tk as tk
+from tkinter import *
 
-window = tk()
+window = Tk()
+window.title("Tagme")
+window.geometry("500x500")
+
+FRAME = Frame(window)
+FRAME.pack()
+
+LOAD_ROW = LabelFrame(FRAME, height=20)
+LOAD_ROW.pack(side=TOP)
+LOAD_BAR_TEXT = Label(LOAD_ROW, text="Path to file: ")
+LOAD_BAR = Entry(LOAD_ROW, width=70)
+LOAD_BAR_TEXT.pack(side=LEFT)
+LOAD_BAR.pack(side=RIGHT)
+
+#COVER_ART = Label(FRAME, bg="red", bitmap=Image(file="..//drawup.png"))
+#COVER_ART.pack(side=LEFT)
 
 window.mainloop()
