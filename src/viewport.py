d57fe5588c04a4da44f83bd0a6669b576896c92c
Zefaxet <getmemedkid@dicksinhisan.us>
2018-06-05 14:16:00 -0500

    Splitting the viewport into the sections for organizing according to the drawup.

--- src/viewport.py
+++ src/viewport.py
@@ -4,17 +4,35 @@ window = Tk()
 window.title("Tagme")
 window.geometry("500x500")
 
-FRAME = Frame(window)
-FRAME.pack()
+# Top level frame, whole window
+MAIN_FRAME = Frame(window)
+MAIN_FRAME.pack(expand=1, fill=BOTH)
 
-LOAD_ROW = LabelFrame(FRAME, height=20)
+# Next level frames, the search bar area and a frame below it for the rest of the window
+LOAD_ROW = LabelFrame(MAIN_FRAME, height=20)
 LOAD_ROW.pack(side=TOP)
+MUTATION_AREA = LabelFrame(MAIN_FRAME)
+MUTATION_AREA.pack(side=BOTTOM, expand=1, fill=BOTH)
+
+# Widgets in the search area
 LOAD_BAR_TEXT = Label(LOAD_ROW, text="Path to file: ")
 LOAD_BAR = Entry(LOAD_ROW, width=70)
 LOAD_BAR_TEXT.pack(side=LEFT)
 LOAD_BAR.pack(side=RIGHT)
 
-#COVER_ART = Label(FRAME, bg="red", bitmap=Image(file="..//drawup.png"))
-#COVER_ART.pack(side=LEFT)
+# Mutation area frame split
+TOP_INFO = LabelFrame(MUTATION_AREA)
+TOP_INFO.pack(side=TOP, expand=1, fill=BOTH)
+LOWER_INFO = LabelFrame(MUTATION_AREA)
+LOWER_INFO.pack(side=BOTTOM, expand=1, fill=BOTH)
+
+# Top level frame split
+ART_FRAME = LabelFrame(TOP_INFO)
+ART_FRAME.master.size()
+ART_FRAME.pack(side=LEFT, expand=1, fill=BOTH, padx=5)
+TOP_INFO_ENTRY = LabelFrame(TOP_INFO)
+TOP_INFO_ENTRY.pack(side=RIGHT, expand=1, fill=BOTH)
+# COVER_ART = Label(MAIN_FRAME, bg="red", bitmap=Image(file="..//drawup.png"))
+# COVER_ART.pack(side=LEFT)
 
 window.mainloop()
