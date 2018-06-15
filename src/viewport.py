a777299af15437361cbad7a5e49b8db5f823281d
zefaxet <zefaxet@gmail.com>
2018-06-15 15:38:07 -0500

    Load area

--- src/viewport.py
+++ src/viewport.py
@@ -3,48 +3,48 @@ sys.path.append(r'C:\Python27\Lib')
 import clr
 clr.AddReference("System.Windows.Forms")
 clr.AddReference("System.Drawing")
-from System.Windows.Forms import Application, Form, Label, Button
-from System.Drawing import Point
+from System.Windows.Forms import Application, FormBorderStyle, Form, Panel, BorderStyle, Label, Button, TextBox, DockStyle
+from System.Drawing import Size, Color
 
-count = 0
+window = Form()
+window.Text = "tagme"
+window.Name = "tagme"
+window.Size = Size(500,500)
+window.FormBorderStyle = FormBorderStyle.FixedDialog
+
+LOAD_AREA = Panel()
+#LOAD_AREA.BorderStyle = BorderStyle.FixedSingle
+LOAD_AREA.Width = window.ClientRectangle.Width
+LOAD_AREA.Height = 20
+LOAD_AREA.Dock = DockStyle.Top
+window.Controls.Add(LOAD_AREA)
+
+test = Panel()
+test.Width = LOAD_AREA.Width
+test.Height = LOAD_AREA.Height
+test.BorderStyle = BorderStyle.FixedSingle
+test.Dock = DockStyle.Fill
+test.BackColor = Color.Red
+window.Controls.Add(test)
+
+LOAD_BUTTON = Button()
+LOAD_BUTTON.Text = "Load"
+LOAD_BUTTON.Dock = DockStyle.Right
+LOAD_TEXTBOX = TextBox()
+LOAD_TEXTBOX.Dock = DockStyle.Left
+LOAD_TEXTBOX.Width = LOAD_AREA.Width - LOAD_BUTTON.Width
+LOAD_AREA.Controls.Add(LOAD_BUTTON)
+LOAD_AREA.Controls.Add(LOAD_TEXTBOX)
 
 
-def clicked(sender, args):
-    global count
-    global button
-    count += 1
-    button.Text = "Clicked %s times" % count
 
-
-a = Form()
-a.Text = "Hi"
-a.Name = "Hi"
-
-label = Label()
-label.Text = "lel"
-label.Location = Point(50,50)
-label.Height = 30
-label.Width = 200
-a.Controls.Add(label)
-
-button = Button()
-button.Text="yeman"
-button.Location = Point(100,100)
-button.Height = 30
-button.Width = 50
-button.Click += clicked
-a.Controls.Add(button)
-
-Application.Run(a)
+Application.EnableVisualStyles()
+Application.Run(window)
 
 
 # window = Tk()
 # window.title("Tagme")
 # window.geometry("500x500")
-#
-# # Top level frame, whole window
-# MAIN_FRAME = Frame(window)
-# MAIN_FRAME.pack(expand=1, fill=BOTH)
 #
 # # Next level frames, the search bar area and a frame below it for the rest of the window
 # LOAD_ROW = LabelFrame(MAIN_FRAME, height=20)
