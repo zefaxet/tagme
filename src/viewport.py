2b6a7595782584746919478ea246da9c69b103ed
zefaxet <zefaxet@gmail.com>
2018-06-15 23:58:16 -0500

    Got an image in. This will later be populated by the album cover.

--- src/viewport.py
+++ src/viewport.py
@@ -3,8 +3,8 @@ sys.path.append(r'C:\Python27\Lib')
 import clr
 clr.AddReference("System.Windows.Forms")
 clr.AddReference("System.Drawing")
-from System.Windows.Forms import Application, FormBorderStyle, Form, Panel, BorderStyle, Label, Button, TextBox, DockStyle
-from System.Drawing import Size, Color
+from System.Windows.Forms import Application, FormBorderStyle, Form, Panel, BorderStyle, Label, Button, TextBox, DockStyle, PictureBox, PictureBoxSizeMode
+from System.Drawing import Size, Color, Image
 
 window = Form()
 window.Text = "tagme"
@@ -12,6 +12,7 @@ window.Name = "tagme"
 window.Size = Size(500,500)
 window.FormBorderStyle = FormBorderStyle.FixedDialog
 
+#  TOP LEVEL CONTROLS ###################
 LOAD_AREA = Panel()
 #LOAD_AREA.BorderStyle = BorderStyle.FixedSingle
 LOAD_AREA.Width = window.ClientRectangle.Width
@@ -19,45 +20,72 @@ LOAD_AREA.Height = 20
 LOAD_AREA.Dock = DockStyle.Top
 window.Controls.Add(LOAD_AREA)
 
-test = Panel()
-test.Width = LOAD_AREA.Width
-test.Height = LOAD_AREA.Height
-test.BorderStyle = BorderStyle.FixedSingle
-test.Dock = DockStyle.Fill
-test.BackColor = Color.Red
-window.Controls.Add(test)
+MUTATION_AREA = Panel()
+MUTATION_AREA.Width = LOAD_AREA.Width
+MUTATION_AREA.Height = LOAD_AREA.Height
+#MUTATION_AREA.BorderStyle = BorderStyle.FixedSingle
+MUTATION_AREA.Dock = DockStyle.Fill
+#MUTATION_AREA.BackColor = Color.Red
+window.Controls.Add(MUTATION_AREA)
+#########################################
 
+#  LOAD FILE CONTROLS ###################
 LOAD_BUTTON = Button()
 LOAD_BUTTON.Text = "Load"
 LOAD_BUTTON.Dock = DockStyle.Right
+
 LOAD_TEXTBOX = TextBox()
 LOAD_TEXTBOX.Dock = DockStyle.Left
 LOAD_TEXTBOX.Width = LOAD_AREA.Width - LOAD_BUTTON.Width
+
 LOAD_AREA.Controls.Add(LOAD_BUTTON)
 LOAD_AREA.Controls.Add(LOAD_TEXTBOX)
+#########################################
 
+#  MUTATION AREA CONTROLS ###############
+EXPLORER_VISIBLE_INFORMATION_AREA = Panel()
+EXPLORER_VISIBLE_INFORMATION_AREA.Height = MUTATION_AREA.Height / 2
+EXPLORER_VISIBLE_INFORMATION_AREA.Dock = DockStyle.Top
 
+UNDERLYING_INFORMATION_AREA = Panel()
+UNDERLYING_INFORMATION_AREA.Height = MUTATION_AREA.Height / 2
+UNDERLYING_INFORMATION_AREA.Dock = DockStyle.Bottom
+UNDERLYING_INFORMATION_AREA.BackColor = Color.Black
+
+MUTATION_AREA.Controls.Add(UNDERLYING_INFORMATION_AREA)
+MUTATION_AREA.Controls.Add(EXPLORER_VISIBLE_INFORMATION_AREA)
+#########################################
+
+#  EXPLORER INFO AREA CONTROLS ##########
+ART_AREA = Panel()
+ART_AREA.Width = EXPLORER_VISIBLE_INFORMATION_AREA.Width * 0.4
+ART_AREA.Dock = DockStyle.Left
+
+INFO_AREA = Panel()
+INFO_AREA.Width = EXPLORER_VISIBLE_INFORMATION_AREA.Width * 0.6
+INFO_AREA.Dock = DockStyle.Right
+INFO_AREA.BackColor = Color.Blue
+
+EXPLORER_VISIBLE_INFORMATION_AREA.Controls.Add(ART_AREA)
+EXPLORER_VISIBLE_INFORMATION_AREA.Controls.Add(INFO_AREA)
+#########################################
+
+#  ART AREA CONTROLS ####################
+COVER_ART = PictureBox()
+COVER_ART.SizeMode = PictureBoxSizeMode.StretchImage
+COVER_ART.Image = Image.FromFile('../drawup.png')
+COVER_ART.Size = Size(160,160)
+COVER_ART.Top = 45
+COVER_ART.Left = (ART_AREA.Width - COVER_ART.Width) / 2
+
+ART_AREA.Controls.Add(COVER_ART)
+#########################################
 
 Application.EnableVisualStyles()
 Application.Run(window)
 
 
-# window = Tk()
-# window.title("Tagme")
-# window.geometry("500x500")
-#
-# # Next level frames, the search bar area and a frame below it for the rest of the window
-# LOAD_ROW = LabelFrame(MAIN_FRAME, height=20)
-# LOAD_ROW.pack(side=TOP)
-# MUTATION_AREA = LabelFrame(MAIN_FRAME)
-# MUTATION_AREA.pack(side=BOTTOM, expand=1, fill=BOTH)
-#
-# # Widgets in the search area
-# LOAD_BAR_TEXT = Label(LOAD_ROW, text="Path to file: ")
-# LOAD_BAR = Entry(LOAD_ROW, width=70)
-# LOAD_BAR_TEXT.pack(side=LEFT)
-# LOAD_BAR.pack(side=RIGHT)
-#
+
 # # Mutation area frame split
 # TOP_INFO = LabelFrame(MUTATION_AREA)
 # TOP_INFO.pack(side=TOP, expand=1, fill=BOTH)
