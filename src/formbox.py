232bc25cabb9a9e4b3286714d950709c59be4677
zefaxet <zefaxet@gmail.com>
2018-06-19 21:44:44 -0500

    Formbox now handles placeholder text

--- src/formbox.py
+++ src/formbox.py
@@ -3,10 +3,28 @@ sys.path.append(r'C:\Python27\Lib')
 import clr
 
 clr.AddReference("System.Windows.Forms")
-from System.Windows.Forms import TextBox
+from System.Windows.Forms import TextBox, HorizontalAlignment
+clr.AddReference("System.Drawing")
+from System.Drawing import Color
 
 class formbox(TextBox):
 
     def __init__(self, placeholdertext):
         TextBox.__init__(self)
-        self.Text = placeholdertext
+        self.Enter += self.recieve_focus
+        self.Leave += self.lose_focus
+        self.__placeholder = placeholdertext
+        self.lose_focus(None, None)
+
+
+    def recieve_focus(self, sender, args):
+        if self.TextAlign == HorizontalAlignment.Center:
+            self.Text = ""
+            self.TextAlign = HorizontalAlignment.Left
+            self.ForeColor = Color.Black
+
+    def lose_focus(self, sender, args):
+        if self.Text == "":
+            self.Text = self.__placeholder
+            self.TextAlign = HorizontalAlignment.Center
+            self.ForeColor = Color.Gray
