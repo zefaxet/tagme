7b94203724d33f490f13836c959238275176b2c2
zefaxet <zefaxet@gmail.com>
2018-07-16 23:28:35 -0500

    What you currently have IN YOUR MOUTH, is ART!

--- src/viewport.py
+++ src/viewport.py
@@ -25,6 +25,7 @@ from System.IO import MemoryStream
 import sys
 from util.log import create_logger
 from widget.formbox import Formbox
+from widget.art import *
 from FileInterface import FileInterface
 
 # endregion
@@ -35,7 +36,6 @@ sys.path.append(r'C:\Python27\Lib')
 
 LOGGER = create_logger('Viewport')
 FORMBOXES = {}
-ART_SELECTOR_LIST_ITEMS = ["Front Cover", "Back Cover"]
 FI = None
 
 LOGGER.info("Booting viewport.")
@@ -105,25 +105,12 @@ EXPLORER_VISIBLE_INFORMATION_AREA.Controls.Add(ART_AREA)
 EXPLORER_VISIBLE_INFORMATION_AREA.Controls.Add(INFO_AREA)
 #########################################
 
-# TODO add dropdown to modify each of the album art tags
 #  ART AREA CONTROLS ####################
-ART_SELECTOR = ComboBox()
-ART_SELECTOR.Size = Size(100, 20)
-ART_SELECTOR.Top = 26
-ART_SELECTOR.Left = (ART_AREA.Width - ART_SELECTOR.Width) / 2
-ART_SELECTOR.DropDownStyle = ComboBoxStyle.DropDownList
-for item in ART_SELECTOR_LIST_ITEMS:
-    ART_SELECTOR.Items.Add(item)
-ART_SELECTOR.SelectedItem = ART_SELECTOR.Items[0]
-ART_SELECTOR.Enabled = False
+ART_SELECTOR = Selector(ID3_TAGS.keys(), Size(100, 20), 26, (ART_AREA.Width - 100) / 2)
 
 # TODO Create new class for this widget
-ART = PictureBox()
-ART.SizeMode = PictureBoxSizeMode.StretchImage
+ART = Art()
 #  ART.Image = Bitmap(MemoryStream(WebClient().DownloadData('https://upload.wikimedia.org/wikipedia/en/2/2c/Metallica_-_Metallica_cover.jpg')))
-ART.BackColor = Color.White
-ART.Size = Size(160, 160)
-ART.Top = 50
 ART.Left = (ART_AREA.Width - ART.Width) / 2
 
 ART_AREA.Controls.Add(ART_SELECTOR)
@@ -271,61 +258,3 @@ LOAD_TEXTBOX.set_text(r'../test/Roundabout.flac')
 
 Application.EnableVisualStyles()
 Application.Run(window)
-
-#the following codeblock sets a placeholder to each of the picture frames on the loaded file
-#clr.AddReferenceToFileAndPath(r'../lib/taglib-sharp.dll')
-    # from TagLib.Id3v2 import AttachedPictureFrame
-    # from TagLib import Picture, PictureType
-    # import TagLib.Png as png
-    #
-    # # x00 = Picture(r'../../0x00.png')
-    # # x01 = Picture(r'../../0x01.png')
-    # # x02 = Picture(r'../../0x02.png')
-    # # x03 = Picture(r'../../0x03.png')
-    # # x04 = Picture(r'../../0x04.png')
-    # # x05 = Picture(r'../../0x05.png')
-    # # x06 = Picture(r'../../0x06.png')
-    # # x07 = Picture(r'../../0x07.png')
-    # # x08 = Picture(r'../../0x08.png')
-    # # x09 = Picture(r'../../0x09.png')
-    # # x0A = Picture(r'../../0x0A.png')
-    # # x0B = Picture(r'../../0x0B.png')
-    # # x0C = Picture(r'../../0x0C.png')
-    # # x0D = Picture(r'../../0x0D.png')
-    # # x0E = Picture(r'../../0x0E.png')
-    # # x0F = Picture(r'../../0x0F.png')
-    # # x10 = Picture(r'../../0x10.png')
-    # # x11 = Picture(r'../../0x11.png')
-    # # x12 = Picture(r'../../0x12.png')
-    # # x13 = Picture(r'../../0x13.png')
-    # # x14 = Picture(r'../../0x14.png')
-    # # xff = Picture(r'../../0xff.png')
-    # # x00.Type = PictureType.Other
-    # # x01.Type = PictureType.FileIcon
-    # # x02.Type = PictureType.OtherFileIcon
-    # # x03.Type = PictureType.FrontCover
-    # # x04.Type = PictureType.BackCover
-    # # x05.Type = PictureType.LeafletPage
-    # # x06.Type = PictureType.Media
-    # # x07.Type = PictureType.LeadArtist
-    # # x08.Type = PictureType.Artist
-    # # x09.Type = PictureType.Conductor
-    # # x0A.Type = PictureType.Band
-    # # x0B.Type = PictureType.Composer
-    # # x0C.Type = PictureType.Lyricist
-    # # x0D.Type = PictureType.RecordingLocation
-    # # x0E.Type = PictureType.DuringRecording
-    # # x0F.Type = PictureType.DuringPerformance
-    # # x10.Type = PictureType.MovieScreenCapture
-    # # x11.Type = PictureType.ColoredFish
-    # # x12.Type = PictureType.Illustration
-    # # x13.Type = PictureType.BandLogo
-    # # x14.Type = PictureType.PublisherLogo
-    # # xff.Type = PictureType.NotAPicture
-    # #
-    # # clr.AddReference("System")
-    # # from System import Array
-    # #
-    # # pictures = Array[AttachedPictureFrame]([AttachedPictureFrame(x) for x in [x00,x01,x02,x03,x04,x05,x06,x07,x08,x09,x0A,x0B,x0C,x0D,x0E,x0F,x10,x11,x12,x13,x14,xff]])
-    # # FI.set_pictures(pictures)
-    # # FI.file.Save()
